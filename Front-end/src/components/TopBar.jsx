c3ea2cf46e568241c32878b2eccfc4f87aed1f63
GurvanN22 <93199510+GurvanN22@users.noreply.github.com>
2023-03-19 01:16:59 +0100

    update-loginPage(functionnal but minimal css and not api functions)

--- Front-end/src/components/TopBar.jsx
+++ Front-end/src/components/TopBar.jsx
@@ -1,7 +1,10 @@
+import Logo from '../assets/img/react.svg'
 function TopBar() {
 
     return (
       <div className="TopBar">
+        <img src={Logo} alt="Logo de ... " className='TopBar-logo'/>
+        <p>oui</p>
       </div>
     )
   }
