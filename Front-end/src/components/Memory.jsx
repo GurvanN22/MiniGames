68edf76a7cdbed8bded17acef51f8705dce0b02e
kylian <kylianoger@orange.fr>
2023-03-21 16:40:11 +0100

    new-technology of the game (find the pair)

--- Front-end/src/components/Memory.jsx
+++ Front-end/src/components/Memory.jsx
@@ -8,7 +8,12 @@ function Memory(props){
 
 
     function DoubleArray(Array){
-        let DoubleArray = Array.concat(Array)
+        //double the array by copying it and renaming it
+        let DoubleArray =[] 
+        for (let i = 0; i < Array.length; i++) {
+            DoubleArray.push(`${Array[i]}copy`)
+        }
+        DoubleArray = Array.concat(DoubleArray)
         return DoubleArray
     }
 
@@ -17,14 +22,59 @@ function Memory(props){
         return RandomArray
     }
 
+    let CheckCard = []
+
+    function checkDouble(){
+        if (CheckCard.length >= 2){
+            //Attendre 1 seconde
+            setTimeout(() => {
+                //check if the 2 cards are the same first letter 
+                if (CheckCard[0].card[0] === CheckCard[1].card[0]){
+                    console.log('Clique sur 2 cartes identiques')
+                    for (let i = 0; i< CheckCard.length; i++){
+                        document.getElementById(CheckCard[i].card).style.backgroundColor = 'green'
+                        document.getElementById(CheckCard[i].card).disabled = true
+                    }
+                    //checkcard is empty
+                    CheckCard = []
+                }else{
+            for (let i = 0; i< CheckCard.length; i++){
+               document.getElementById(CheckCard[i].card).style.backgroundColor = 'blue'
+            }
+            //checkcard is empty
+            CheckCard = []
+        }
+    }, 1000)
+        }else{
+            console.log('Cliqué sur 2 cartes')
+        }
+    }
 
     let Card =RandomArray(DoubleArray(props.MemoryCard))
+    console.log(Card)
 
-    const DisplayCard = Card.map((card) => 
-        <div className='Card'>
-            <button>{card}</button>
-            </div>
+    const DisplayCard = Card.map((card) =>  
+            <button onClick = { () => checkInformation({card})} className='Cards' id={card}></button>
     )
+
+    
+
+ function checkInformation(a){
+        console.log(a.card)
+        CheckCard.push(a)
+        console.log(CheckCard)
+        document.getElementById(a.card).style.backgroundColor = 'red'
+        checkDouble()
+        
+    
+
+
+
+
+
+
+        
+    }
 
 
     return(
