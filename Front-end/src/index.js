f38e806e28bd1aff14b2cbd7275a6d0c3aae1ca2
Le_Rouge <93199510+GurvanN22@users.noreply.github.com>
2023-03-23 19:28:43 +0100

    stop-using-vite

---
+++ Front-end/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Ways from './Ways'
+import './assets/scss/style.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ways />
+  </React.StrictMode>,
+)
